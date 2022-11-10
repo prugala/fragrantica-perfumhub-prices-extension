fc40ddab3a4f6cf0ed8bdd900f68535a622b6417
Piotr Rugala <piotr@isedo.pl>
2022-11-10 14:53:10 +0100

    parfumo.net support

--- src/content/index.tsx
+++ src/content/index.tsx
@@ -6,4 +6,6 @@ appElement.id = 'prices-box'
 render(<Prices/>, appElement)
 
 const ratingElement = document.getElementById('rating')
+const optionsElement = document.getElementById('p_options_holder')
 ratingElement?.parentNode?.insertBefore(appElement, ratingElement?.nextSibling)
+optionsElement?.parentNode?.insertBefore(appElement, optionsElement?.nextSibling)
